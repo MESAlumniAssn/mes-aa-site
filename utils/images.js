8cc6f8125fb2eac30d671e6e13d016459f437da9
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-07-13 12:13:59 +0530

    Added the prod version of imagekit

--- utils/images.js
+++ utils/images.js
@@ -1,17 +1,14 @@
 export const LOGO =
-  "https://ik.imagekit.io/ykidmzssaww/MES-AA/Site_Images/logo_FCZ2Z5QR1.jpg";
-
-export const HOME =
-  "https://ik.imagekit.io/ykidmzssaww/MES-AA/Site_Images/Home/home_sDrpc5-j8.jpg";
+  "https://ik.imagekit.io/pwxm960evbp/MES-AA/Site_Images/logo_-iztI7b1N.jpg";
 
 export const ABOUT_HEADER =
-  "https://ik.imagekit.io/ykidmzssaww/tr:h-400,w-550/MES-AA/Site_Images/About/college-old_1__dQNkbAIKj.png";
+  "https://ik.imagekit.io/pwxm960evbp/tr:h-400,w-550/MES-AA/Site_Images/About/college-old_7uo2jBfRvW.png";
 
 export const REGISTRATION_CERTIFICATE =
-  "https://ik.imagekit.io/ykidmzssaww/tr:h-600,w-400/MES-AA/Site_Images/About/registration_1__L6pHU90dX.jpeg";
+  "https://ik.imagekit.io/pwxm960evbp/tr:h-600,w-400/MES-AA/Site_Images/About/registration_mcEtHRz3r.jpeg";
 
 export const DEFAULT_MC =
-  "https://ik.imagekit.io/ykidmzssaww/tr:h-255,w-245/MES-AA/Site_Images/About/Committee/default-mc_p8Hod0Dqe.svg";
+  "https://ik.imagekit.io/pwxm960evbp/tr:h-255,w-245/MES-AA/Site_Images/About/Committee/default-mc_ttHpAvGdL.svg";
 
 export const CONTACT_HEADER =
-  "https://ik.imagekit.io/ykidmzssaww/tr:w-600/MES-AA/Site_Images/Contact/contact_83gH2Qv-h.jpg";
+  "https://ik.imagekit.io/pwxm960evbp/tr:w-600/MES-AA/Site_Images/Contact/contact_RFmxIu1-F.jpg";
